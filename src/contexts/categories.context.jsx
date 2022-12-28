6cc33b9cec5cb3e7be3aa9522a4389a9cbb81ac9
Piriya <piriya.muaithaisong@gmail.com>
2022-12-28 23:03:59 +0700

    init graphql categories

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,22 +1,42 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
+
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
+  const {loading, error, data} = useQuery(COLLECTIONS);
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+  
 
-    getCategoriesMap();
-  }, []);
+  // useEffect(() => {
+  //   const getCategoriesMap = async () => {
+  //     const categoryMap = await getCategoriesAndDocuments();
+  //     setCategoriesMap(categoryMap);
+  //   };
+
+  //   getCategoriesMap();
+  // }, []);
 
   const value = { categoriesMap };
   return (
